79aefc9c21bc817174c33d1b03b725dc2847fe18
한일석 <x2ever@naver.com>
2019-12-31 23:20:39 +0900

    Update Env.py

--- gym_indoorAutoDriving/envs/v_0/Env.py
+++ gym_indoorAutoDriving/envs/v_0/Env.py
@@ -90,7 +90,7 @@ class indoorAutoDrivingEnv(gym.Env):
             done = True
             reward = -1
 
-        if self.without_reward > 500:
+        if self.without_reward > self.map_size * self.map_size / 4:
             done = True
             reward = 0
 
